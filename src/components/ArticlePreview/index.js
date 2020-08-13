3518f5575085816a80147c3c986f176b441cc2af
Laetitia <l.cocusse@gmail.com>
2020-08-13 15:50:43 +0200

    add main for mainArticlePreview

--- src/components/ArticlePreview/index.js
+++ src/components/ArticlePreview/index.js
@@ -2,18 +2,20 @@ import React from 'react'
 import styles from './ArticlePreview.module.css'
 
 function ArticlePreview(props) {
+    const mainClass = props.isMain ? styles.main : ''
+    
     return (
         <div className={styles.basicArticle}>
             <div className={styles.sideLeft}>
-                <div className={styles.tag}>{props.tag}</div>
-                <img src={props.image}></img>
+                <div className={`${styles.tag} ${mainClass}`}>{props.tag} </div>
+                <img src={props.image} className={`${styles.img} ${mainClass}`}></img>
             </div>
 
-            <div className={styles.sideRight}>
-                <h1>{props.title}</h1>
-                <div className={styles.date}>{props.date}</div>
-                <div className={styles.description}>{props.description}</div>
-                <div className={styles.link}><a href={props.link}>Lire l'article</a></div>
+            <div className={`${styles.sideRight} ${mainClass}`}>
+                <h1 className={`${styles.title} ${mainClass} `}>{props.title}</h1> 
+                <div className={`${styles.date} ${mainClass}`}>{props.date}</div>
+                <div className={`${styles.description} ${mainClass}`}>{props.description}</div>
+                <div className={`${styles.link} ${mainClass}`}><a href={props.link}>Lire l'article</a></div>
             </div>
         </div>
     )
