461df913afa71d2161ecea105d8e6709472274a3
Laetitia Cocusse <l.cocusse@gmail.com>
2020-08-26 14:38:08 +0200

    Feature/header (#20)
    * Add skeleton for navbar
    
    * refacto navbar function and stories, and add css
    
    * add uppercase text and remove flex on navBar
    
    * update CSS for navigationBar and navItem
    
    * fix space and remove comment
    
    * Add skeleton for header

---
+++ src/components/Header/Header.stories.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from './index'
+
+export default {
+    title : "Header",
+    component : Header
+}
+
+const header1 = {
+    logo: "/images/petit-pot.png",
+    navItems: [
+        { title: "Activités", link: "#" },
+        { title: "Cuisine", link: "#" },
+        { title: "Kawaii", link: "#" },
+        { title: "Mum and kid", link: "#" },
+        { title: "Prendre l'air", link: "#" },
+    ]
+
+}
+
+export const BasicHeader = () => <Header {...header1} />
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import styles from './Header.module.css'
+
+
+function Header(props) {
+    return (
+        <div className={styles.header}>
+            <img src={props.logo} className={styles.logo}></img>
+            <Navbar navItems={props.navItems}/>
+        </div>
+    )
+}
+
+export default Header
