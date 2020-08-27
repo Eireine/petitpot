928c41081deeb5304fe991ad4eb449e7e85bed9b
Laetitia Cocusse <l.cocusse@gmail.com>
2020-08-27 09:12:36 +0200

    Tooling/linter (#27)
    * Add and configure eslint and prettier
    
    * Eslint fixes
    
    * Ignore some rules
    
    * Eslint fix

--- src/components/Navbar/Navbar.stories.js
+++ src/components/Navbar/Navbar.stories.js
@@ -2,18 +2,18 @@ import React from 'react'
 import Navbar from './index'
 
 export default {
-    title : "Navbar",
-    component : Navbar
+  title: 'Navbar',
+  component: Navbar
 }
 
 const navbar1 = {
-    navItems: [
-        { title: "Activités", link: "#" },
-        { title: "Cuisine", link: "#" },
-        { title: "Kawaii", link: "#" },
-        { title: "Mum and kid", link: "#" },
-        { title: "Prendre l'air", link: "#" },
-    ]
-} 
+  navItems: [
+    { title: 'Activités', link: '#' },
+    { title: 'Cuisine', link: '#' },
+    { title: 'Kawaii', link: '#' },
+    { title: 'Mum and kid', link: '#' },
+    { title: "Prendre l'air", link: '#' }
+  ]
+}
 
 export const BasicNavbar = () => <Navbar {...navbar1} />
